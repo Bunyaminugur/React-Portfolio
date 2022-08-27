6e57bf1dbd91e3b72b087e69658097e6cc2b8c50
Bunyaminugur <bunyamin26@outlook.com>
2022-08-27 17:30:26 +0200

    Add navbar extra sticky links on left side of page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes} from 'react-icons/fa'
+import React, { useState } from 'react';
+import { FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill} from 'react-icons/bs';
 
 function Navbar() {
     const [show, setShow] = useState(false)
@@ -27,6 +29,31 @@ function Navbar() {
             <li className='py-6 text-4xl'>Work</li>
             <li className='py-6 text-4xl'>Contact</li>
         </ul>
+
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-700'>
+                    <a className='flex justify-between items-center w-full text-gray-200' href='/'>
+                        Linkedin <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-700'>
+                    <a className='flex justify-between items-center w-full text-gray-200' href='/'>
+                        Github <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                    <a className='flex justify-between items-center w-full text-gray-200' href='/'>
+                        Email <HiOutlineMail size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-500'>
+                    <a className='flex justify-between items-center w-full text-gray-200' href='/'>
+                        Resume <BsFillPersonLinesFill size={30}/>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
