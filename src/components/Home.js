dde5a7fdbed73abb20ad2630980f3b406b7f779d
Bunyaminugur <bunyamin26@outlook.com>
2022-08-28 01:09:41 +0200

    Add home components and style

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,23 @@ import { HiArrowNarrowRight} from 'react-icons/hi'
 
 function Home() {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Bunyamin Ugur</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w[700px]'>make random sentcen this it the placce that you put your things in it
+               dont worrk keeepp asik iska tuern
+            </p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects
+                <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3'/>
+                </span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
